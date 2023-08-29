19c98f7e3933dc7318d354ec71b9c60eb35784b6
Francislie <mfrancislie@gmail.com>
2023-08-29 12:11:48 -0700

    Navbar Component

---
+++ src/data.jsx
@@ -0,0 +1,24 @@
+import { nanoid } from 'nanoid';
+
+export const Links = [
+  {
+    id: nanoid(),
+    href: '#home',
+    text: 'home',
+  },
+  {
+    id: nanoid(),
+    href: '#skills',
+    text: 'skills',
+  },
+  {
+    id: nanoid(),
+    href: '#about',
+    text: 'about',
+  },
+  {
+    id: nanoid(),
+    href: '#projects',
+    text: 'projects',
+  },
+];
